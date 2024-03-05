4dd7813b9967e1fde86e47b684eb4db98257f12c
zaher-aa <zaherabuamro@gmail.com>
2024-03-05 12:36:38 +1100

    f

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,9 +8,6 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  'https://raw.githubusercontent.com/zaher-aa/zaher-aa/main/Zaher-Abuamro-Resume.pdf';
-
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
@@ -35,9 +32,6 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
         </Row>
       </Container>
     </div>
